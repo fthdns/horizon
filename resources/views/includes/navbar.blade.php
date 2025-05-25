f36ee1d09a91e1a3085dbe1c1265db435000380d
Fatih Daffa <fatihdaffa9@student.ub.ac.id>
2025-05-25 20:43:39 +0700

    add authentication for user and admin

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -32,26 +32,54 @@
                     </li>
                 </ul>
 
+                @guest
                 <!-- Mobile Button -->
                 <form action="" class="form-inline d-sm-block d-md-none mx-2 my-2">
-                    <button class="btn btn-login my-2 my-sm-0">
-                        Masuk
+                    <button class="btn btn-login my-2 my-sm-0" type="button" onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+                        Login
                     </button>
                 </form>
 
                 <!-- Tab Button -->
                 <form action="" class="form-inline d-none d-md-block d-lg-none mx-2 my-2">
-                    <button class="btn btn-login my-2 my-sm-0">
-                        Masuk
+                    <button class="btn btn-login my-2 my-sm-0" type="button" onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+                        Login
                     </button>
                 </form>
 
                 <!-- Desktop Button -->
                 <form action="" class="form-inline my-2 my-lg-0 d-none d-lg-block">
-                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
-                        Masuk
+                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="button" onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+                        Login
                     </button>
                 </form>
+                @endguest
+
+                @auth
+                <!-- Mobile Button -->
+                <form class="form-inline d-sm-block d-md-none mx-2 my-2" action="{{ url('logout') }}" method="POST">
+                    @csrf
+                    <button class="btn btn-login my-2 my-sm-0" type="submit">
+                        Log out
+                    </button>
+                </form>
+
+                <!-- Tab Button -->
+                <form class="form-inline d-none d-md-block d-lg-none mx-2 my-2" action="{{ url('logout') }}" method="POST">
+                    @csrf
+                    <button class="btn btn-login my-2 my-sm-0" type="submit">
+                        Log out
+                    </button>
+                </form>
+
+                <!-- Desktop Button -->
+                <form class="form-inline my-2 my-lg-0 d-none d-lg-block" action="{{ url('logout') }}" method="POST">
+                    @csrf
+                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="submit">
+                        Log out
+                    </button>
+                </form>
+                @endauth
             </div>
         </nav>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Middleware\IsAdmin;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/detail', [DetailController::class, 'index'])->name('detail');
@@ -12,10 +14,10 @@ Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout-success');
 
 Route::prefix('admin')
+    ->middleware(['auth',IsAdmin::class])
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     });
 
+Auth::routes(['verify' => true]);
 
-
-    
